7060864c90f6d019cd979090b606906b2474521c
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2022-03-09 10:31:44 +0530

    es5 functional inheritance

--- javascript advance concepts/__proto__/es5FunctionInheritence.js
+++ javascript advance concepts/__proto__/es5FunctionInheritence.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * https://medium.com/@dange.laxmikant/simplified-inheritance-in-js-es5-way-60b4ff19b008
+ */
+
+// Basic Example without prototype
 function car(brand) {
     this.brand = brand
 
@@ -7,10 +13,46 @@ function car(brand) {
 }
 
 function model(model, brand) {
-    //to initialize superclass-derived members.
+    //to initialize superclass-derived members. same as super in constructor
     car.call(this, brand);
 
     this.model = model
 }
 
 console.log(new model('mustang','ford'))
+
+// Example with proptypal(functional) inheritance in es5 
+function person(fistName, lastName) {
+    this.firstName = fistName;
+    this.lastName = lastName;
+}
+
+person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`
+}
+
+const pObj = new person('harsha', 'nerella');
+console.log(pObj.getFullName())
+
+// emp inherits person
+function empDetails(empId, firstName, lastName) {
+    person.call(this, firstName, lastName)
+    this.empId = empId  
+    console.log(this.lastName)
+}
+
+// to inherit prototype propertiess from parent
+empDetails.prototype = Object.create(person.prototype)
+// As we copied prototype from parent class(person) we lost constructor of child class(empDetails)
+// to recreate constructor for child do below line
+empDetails.prototype.constructor = empDetails;
+
+empDetails.prototype.getEmpDetails = function() {
+    return [this.empId, this.firstName, this.lastName]
+}
+
+const empO = new empDetails(1, 'h1', 'n1')
+
+console.log(empO.getFullName())
+console.log(empO.getEmpDetails())
+
