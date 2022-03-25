7523378c225766afec28bf9a74ceb3faad2a091d
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2022-03-25 08:16:30 +0530

    mergeTwoSortedArraysUsingMergeSort

---
+++ leetcode problems/arrays/mergeTwoSortedArrays2.js
@@ -0,0 +1,30 @@
+/**
+ *  merge two sorted array using merge sort 
+ *  i < j --> push i and i++; i > j --> push j and j++ ; i===j push i or j and i or j ++
+ * 
+ *  [1,2,3] [2,5,6]  
+ *   i       j
+ * 
+ *   stack: [1] 1<2 i++
+ *  [1,2,3] [2,5,6] 
+ *     i     j
+ *  stack: [1,2] 2==2 j++
+ * 
+ *  [1,2,3] [2,5,6] 
+ *     i     j
+ *  stack: [1,2] 2==2 i++
+ * 
+ *  [1,2,3] [2,5,6] 
+ *       i   j
+ *  stack: [1,2,2] 3>2 j++
+ * [1,2,3] [2,5,6] 
+ *      i     j
+ *  stack: [1,2,2,3] 5>3 i++
+ *  
+ *  //i reached end push remaining elements in j
+ *  [1,2,3] [2,5,6] 
+ *       i     j
+ *  stack: [1,2,2,3,5,6]
+ *  
+ */
+
