2a4d4bbf9dd863acd010662bd62d032cfe9f5e8c
catraining2018@gmail.com <venkatababu.chekoddi@kanrad.com>
2022-07-06 02:43:43 -0400

    update

--- javascript advance concepts/infiniteScroll.js
+++ javascript advance concepts/infiniteScroll.js
@@ -6,3 +6,43 @@ window.addEventListener('scroll',()=>{
     loadImages();
     }
 })
+
+
+const VirtualizedList = props => {
+    const { numItems, itemHeight, renderItem, windowHeight } = props;
+    const [scrollTop, setScrollTop] = useState(0);
+  
+    const innerHeight = numItems * itemHeight;
+    const startIndex = Math.floor(scrollTop / itemHeight);
+    const endIndex = Math.min(
+      numItems - 1, // don't render past the end of the list
+      Math.floor((scrollTop + windowHeight) / itemHeight)
+    );
+  
+    const items = [];
+    for (let i = startIndex; i <= endIndex; i++) {
+      items.push(
+        renderItem({
+          index: i,
+          style: {
+            position: "absolute",
+            top: `${i * itemHeight}px`,
+            width: "100%"
+          }
+        })
+      );
+    }
+  
+    const onScroll = e => setScrollTop(e.currentTarget.scrollTop);
+  
+    return (
+      <div className="scroll" style={{ overflowY: "scroll" }} onScroll={onScroll}>
+        <div
+          className="inner"
+          style={{ position: "relative", height: `${innerHeight}px` }}
+        >
+          {items}
+        </div>
+      </div>
+    );
+  };
